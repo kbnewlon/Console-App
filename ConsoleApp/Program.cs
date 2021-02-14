63fb2ad8a69fbf17372a4639ed1cb54a9f5042c4
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-14 11:55:04 -0800

    methods

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,17 +6,13 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            int[] luckyNumbers = {4, 8, 15, 16, 23, 42 };
-            string[] friends = new string[5];
-            friends[0] = "Jim";
-            friends[1] = "Jim";
-            friends[2] = "Jim";
-            friends[3] = "Jim";
-            friends[4] = "Jim";
-
-            Console.WriteLine(luckyNumbers[1]);
-
+            SayHi("Kayla", 28);
             Console.ReadLine();
         }
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
     }
+     
 }
