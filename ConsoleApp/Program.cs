c7c41b0c4b93defe95dbedca145dd7b619fa53f9
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-14 10:14:15 -0800

    there once was a man named john

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,10 +6,15 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("    /|");
-            Console.WriteLine("   / |");
-            Console.WriteLine("  /  |");
-            Console.WriteLine(" /___|");
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
+
+            Console.WriteLine("There once was a man named John");
+            Console.WriteLine("He was 35 years old");
+            Console.WriteLine("He really liked the name John");
+            Console.WriteLine("But did't like being 35");
 
             Console.ReadLine();
         }
