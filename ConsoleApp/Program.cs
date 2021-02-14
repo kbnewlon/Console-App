0f790ed06bf36e61b52611c1738feca938c74d6a
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-14 11:25:47 -0800

    simple addition calculator

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,8 +6,12 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            int num = Convert.ToInt32("45");
-            Console.WriteLine(num + 6);
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
             Console.ReadLine();
         }
     }
