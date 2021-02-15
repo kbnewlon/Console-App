ced9c6c8e52a630bc990de6c3d33ab35e3a341e4
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-14 18:24:37 -0800

    functional calculator

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,26 +6,39 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetMax(2, 10, 4));
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if(op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+
             Console.ReadLine();
         }
 
-       static int GetMax(int num1, int num2, int num3)
-        {
-            int result;
-            if (num1 >= num2 && num1 >= num3)
-            {
-                result = num1;
-            }
-            else if(num2 >= num1 && num2 >= num3){
-                result = num2;
-            } else
-            {
-                result = num3;
-            }
-
-            return result;
-        }
+       
+        
     }
      
 }
