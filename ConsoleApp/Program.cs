73f391bd2f1ef8995e877575e0e431649bc5efbc
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-14 18:16:07 -0800

    GetMax method num3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,19 +6,22 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetMax(2, 10));
+            Console.WriteLine(GetMax(2, 10, 4));
             Console.ReadLine();
         }
 
-       static int GetMax(int num1, int num2)
+       static int GetMax(int num1, int num2, int num3)
         {
             int result;
-            if (num1 > num2)
+            if (num1 >= num2 && num1 >= num3)
             {
                 result = num1;
             }
-            else {
+            else if(num2 >= num1 && num2 >= num3){
                 result = num2;
+            } else
+            {
+                result = num3;
             }
 
             return result;
