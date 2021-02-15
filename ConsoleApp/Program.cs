d650129ebceed963d49d271ae6ed0ff8b5f0b8a2
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-14 17:58:23 -0800

    else if statements

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,14 +6,19 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            bool isFemale = false;
+            bool isFemale = true;
+            bool isTall = false;
 
-            if (isFemale)
+            if (isFemale || isTall)
             {
-                Console.WriteLine("You are a badass");
+                Console.WriteLine("You are a tall badass");
+            } else if (isFemale && !isTall) {
+                Console.WriteLine("You are short badass!");
+            } else if (!isFemale && isTall) {
+                Console.WriteLine("You're not a badass but you're tall!");
             } else
             {
-                Console.WriteLine("You are not a badass");
+                Console.WriteLine("You are not a badass AND you're short");
             }
 
             Console.ReadLine();
