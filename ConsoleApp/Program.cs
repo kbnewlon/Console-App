f90ba2e92ad079d806c0dedbca2b3ff2f5c85544
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-15 13:17:21 -0800

    do while loop

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,45 +7,19 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
-            Console.WriteLine(GetDay(7));
-            Console.ReadLine();
-        }
-
-       static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch(dayNum)
+            int index = 6;
+            do
             {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invalid Day Number";
-                    break;
-            }
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5) ;
 
-            return dayName;
+                Console.ReadLine();
         }
+
+       
         
     }
      
 }
+
