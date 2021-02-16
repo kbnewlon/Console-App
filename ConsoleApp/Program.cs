7e983cc37668d71ce32c87774612d9111b4da282
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-16 00:11:08 -0800

    getters and setters

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,13 +7,10 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
 
-            Student student1 = new Student("Jim", "Business", 2.8);
-            Student student2 = new Student("Pam", "Art", 3.6);
-
-            Console.WriteLine(student1.HasHonors());
-            Console.WriteLine(student2.HasHonors());
-
+            Console.WriteLine(avengers.Rating);
             Console.ReadLine();
         }
 
