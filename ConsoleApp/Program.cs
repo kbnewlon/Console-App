716379b5a1c183c312445d88ab42ea6f28a90e56
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-16 00:25:47 -0800

    static class attributes

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,10 +7,12 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
-            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
-            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
 
-            Console.WriteLine(avengers.Rating);
+
+            Console.WriteLine(Song.songCount);
             Console.ReadLine();
         }
 
